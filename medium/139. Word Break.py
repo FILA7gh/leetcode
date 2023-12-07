0c014c47b9cfd8a4faf2f40ee644aa63fe5b649f
Aftandil <akmatbekovvv@gmail.com>
2023-12-07 10:52:59 +0600

    huyna es chestno

---
+++ medium/139. Word Break.py
@@ -0,0 +1,33 @@
+# class Solution:
+#     def wordBreak(s: str, wordDict: list[str]) -> bool:
+#         for i in wordDict:
+#             if i in s:
+#                 s = s.replace(i, '')
+#                 print(s)
+#             else:
+#                 return False
+#         return True
+#
+# hz pochemu net
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        def construct(current, wordDict, memo={}):
+            if current in memo:
+                return memo[current]
+
+            if not current:
+                return True
+
+            for word in wordDict:
+                if current.startswith(word):
+                    new_current = current[len(word):]
+                    if construct(new_current, wordDict, memo):
+                        memo[current] = True
+                        return True
+
+            memo[current] = False
+            return False
+
+        return construct(s, wordDict)
